4945c5040fa071bfa42c8e41f2454e5e0b338276
Jeremy Boetticher <me4jeremyboett@gmail.com>
2024-02-19 15:30:45 -0600

    Added swaps

--- ztg_runtime_lib/src/runtime_structs.rs
+++ ztg_runtime_lib/src/runtime_structs.rs
@@ -1,10 +1,12 @@
 use ink::primitives::AccountId;
 use sp_runtime::{MultiAddress, Perbill};
 
-type Balance = u128;
-type Hash = [u8; 32];
-type Timestamp = u64;
-type BlockNumber = u32;
+pub type Balance = u128;
+pub type Hash = [u8; 32];
+pub type Timestamp = u64;
+pub type BlockNumber = u32;
+
+pub type PoolId = u128;
 
 use crate::primitives::*;
 
@@ -43,7 +45,8 @@ pub enum RuntimeCall {
 
 #[derive(scale::Encode, scale::Decode)]
 pub enum AssetManagerCall {
-    // https://github.com/open-web3-stack/open-runtime-module-library/blob/22a4f7b7d1066c1a138222f4546d527d32aa4047/currencies/src/lib.rs#L129-L131C19
+    /// Transfers an asset from the caller's account to the destination account.
+    /// https://github.com/open-web3-stack/open-runtime-module-library/blob/22a4f7b7d1066c1a138222f4546d527d32aa4047/currencies/src/lib.rs#L129-L131C19
     #[codec(index = 0)]
     Transfer {
         dest: MultiAddress<AccountId, ()>,
@@ -103,6 +106,8 @@ pub enum CourtCall {
         #[codec(compact)] court_id: CourtId,
         commitment_vote: Hash
     },
+    /// Denounce a juror during the voting period for which the commitment vote is known.
+    /// https://github.com/zeitgeistpm/zeitgeist/tree/release-v0.5.0/zrml/court/src/lib.rs#L784
     #[codec(index = 5)]
     DenounceVote {
         #[codec(compact)] court_id: CourtId,
@@ -110,20 +115,28 @@ pub enum CourtCall {
         vote_item: VoteItem,
         salt: Hash
     },
+    /// Reveal the commitment vote of the caller, who is a selected juror.
+    /// https://github.com/zeitgeistpm/zeitgeist/tree/release-v0.5.0/zrml/court/src/lib.rs#L868
     #[codec(index = 6)]
     RevealVote {
         #[codec(compact)] court_id: CourtId,
         vote_item: VoteItem,
         salt: Hash,
     },
+    /// Initiate an appeal for a court
+    /// https://github.com/zeitgeistpm/zeitgeist/tree/release-v0.5.0/zrml/court/src/lib.rs#L957
     #[codec(index = 7)]
     Appeal {
         #[codec(compact)] court_id: CourtId
     },
+    /// Reassign the stakes of the jurors and delegators
+    /// https://github.com/zeitgeistpm/zeitgeist/tree/release-v0.5.0/zrml/court/src/lib.rs#L1046
     #[codec(index = 8)]
     ReassignCourtStakes {
         #[codec(compact)] court_id: CourtId
     },
+    /// Set the yearly inflation rate of the court system.
+    /// https://github.com/zeitgeistpm/zeitgeist/tree/release-v0.5.0/zrml/court/src/lib.rs#L1167
     #[codec(index = 9)]
     SetInflation {
         inflation: Perbill
@@ -133,44 +146,111 @@ pub enum CourtCall {
 #[derive(scale::Encode, scale::Decode)]
 #[cfg_attr(feature = "std", derive(scale_info::TypeInfo))]
 pub enum SwapsCall {
+    /// Exits a pool.
+    /// https://github.com/zeitgeistpm/zeitgeist/tree/release-v0.5.0/zrml/swaps/src/lib.rs#L125
     #[codec(index = 1)]
     PoolExit {
         #[codec(compact)]
-        pool_id: u128,
+        pool_id: PoolId,
         #[codec(compact)]
-        pool_amount: u128,
-        min_assets_out: ink::prelude::vec::Vec<u128>,
+        pool_amount: Balance,
+        min_assets_out: ink::prelude::vec::Vec<Balance>,
     },
+    /// Exits a pool with an exact asset amount.
+    /// https://github.com/zeitgeistpm/zeitgeist/tree/release-v0.5.0/zrml/swaps/src/lib.rs#L155
+    #[codec(index = 3)]
+    PoolExitWithExactAssetAmount {
+        #[codec(compact)]
+        pool_id: PoolId,
+        asset: ZeitgeistAsset, // TODO: figure out of AssetOf<T> is this value
+        #[codec(compact)]
+        asset_amount: Balance,
+        #[codec(compact)]
+        max_pool_amount: Balance
+    },
+    /// Exits a pool with an exact pool amount.
+    /// https://github.com/zeitgeistpm/zeitgeist/tree/release-v0.5.0/zrml/swaps/src/lib.rs#L193
+    #[codec(index = 4)]
+    PoolExitWithExactPoolAmount {
+        #[codec(compact)]
+        pool_id: PoolId,
+        asset: ZeitgeistAsset,
+        #[codec(compact)]
+        pool_amount: Balance,
+        #[codec(compact)]
+        min_asset_amount: Balance,
+    },
+    /// Joins a pool.
+    /// https://github.com/zeitgeistpm/zeitgeist/tree/release-v0.5.0/zrml/swaps/src/lib.rs#L266
     #[codec(index = 5)]
     PoolJoin {
         #[codec(compact)]
-        pool_id: u128,
+        pool_id: PoolId,
         #[codec(compact)]
-        pool_amount: u128,
-        max_assets_in: ink::prelude::vec::Vec<u128>,
+        pool_amount: Balance,
+        max_assets_in: ink::prelude::vec::Vec<Balance>,
+    },
+    /// Joins a pool with an exact asset amount.
+    /// https://github.com/zeitgeistpm/zeitgeist/tree/release-v0.5.0/zrml/swaps/src/lib.rs#L318
+    #[codec(index = 7)]
+    PoolJoinWithExactAssetAmount {
+        #[codec(compact)]
+        pool_id: PoolId,
+        asset_in: ZeitgeistAsset,
+        #[codec(compact)]
+        asset_amount: Balance,
+        #[codec(compact)]
+        min_pool_amount: Balance,
+    },
+    /// Joins a pool with an exact pool amount.
+    /// https://github.com/zeitgeistpm/zeitgeist/tree/release-v0.5.0/zrml/swaps/src/lib.rs#L357
+    #[codec(index = 8)]
+    PoolJoinWithExactPoolAmount {
+        #[codec(compact)]
+        pool_id: PoolId,
+        asset: ZeitgeistAsset,
+        #[codec(compact)]
+        pool_amount: Balance,
+        #[codec(compact)]
+        max_asset_amount: Balance
     },
     // https://polkadot.js.org/apps/?rpc=wss%3A%2F%2Fbsr.zeitgeist.pm#/extrinsics/decode/0x380981040402286bee00b102000000000000000000000000000001000100cdbe7b00000000000000000000000000
+    /// Swaps an exact amount in.
+    /// https://github.com/zeitgeistpm/zeitgeist/tree/release-v0.5.0/zrml/swaps/src/lib.rs#L422
     #[codec(index = 9)]
     SwapExactAmountIn {
         #[codec(compact)]
-        pool_id: u128,
+        pool_id: PoolId,
         asset_in: ZeitgeistAsset,
         #[codec(compact)]
-        asset_amount_in: u128,
+        asset_amount_in: Balance,
         asset_out: ZeitgeistAsset,
-        min_asset_amount_out: Option<u128>,
-        max_price: Option<u128>,
+        min_asset_amount_out: Option<Balance>,
+        max_price: Option<Balance>,
     },
+    /// Swaps an exact amount out.
+    /// https://github.com/zeitgeistpm/zeitgeist/tree/release-v0.5.0/zrml/swaps/src/lib.rs#L465
     #[codec(index = 10)]
     SwapExactAmountOut {
         #[codec(compact)]
-        pool_id: u128,
+        pool_id: PoolId,
         asset_in: ZeitgeistAsset,
         max_asset_amount_in: Option<u128>,
         asset_out: ZeitgeistAsset,
         #[codec(compact)]
         asset_amount_out: u128,
         max_price: Option<u128>,
+    },
+    /// Forces a pool exit.
+    /// https://github.com/zeitgeistpm/zeitgeist/tree/release-v0.5.0/zrml/swaps/src/lib.rs#L492
+    #[codec(index = 11)]
+    ForcePoolExit {
+        who: AccountId,
+        #[codec(compact)]
+        pool_id: PoolId,
+        #[codec(compact)]
+        pool_amount: Balance,
+        min_assets_out: ink::prelude::vec::Vec<Balance>,
     },
 }
 
@@ -182,7 +262,7 @@ pub enum PredictionMarketsCall {
         #[codec(compact)]
         market_id: u128,
         #[codec(compact)]
-        amount: u128,
+        amount: Balance,
     },
     #[codec(index = 8)]
     CreateMarket {
@@ -202,9 +282,9 @@ pub enum PredictionMarketsCall {
         #[codec(compact)]
         market_id: u128,
         #[codec(compact)]
-        swap_fee: u128,
+        swap_fee: Balance,
         #[codec(compact)]
-        amount: u128,
+        amount: Balance,
     },
     #[codec(index = 12)]
     RedeemShares {
@@ -216,7 +296,7 @@ pub enum PredictionMarketsCall {
         #[codec(compact)]
         market_id: u128,
         #[codec(compact)]
-        amount: u128,
+        amount: Balance,
     },
 }
 
