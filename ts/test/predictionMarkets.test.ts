ee09c37a005a4fe14d42d4de6a1f12ccb650d8b3
Jeremy Boetticher <me4jeremyboett@gmail.com>
2024-03-16 16:09:46 -0500

    Added 2 prediction market tests

--- ts/test/predictionMarkets.test.ts
+++ ts/test/predictionMarkets.test.ts
@@ -4,40 +4,45 @@ import { ChildProcess } from 'child_process';
 import { ApiPromise, Keyring, WsProvider } from '@polkadot/api';
 import { ContractPromise } from '@polkadot/api-contract';
 import { cryptoWaitReady } from '@polkadot/util-crypto';
-import { CreateMarketParams, ZTG, create, createStorage, localhostRpc } from "@zeitgeistpm/sdk";
+import { CreateMarketParams, MetadataStorage, RpcContext, Sdk, ZTG, create, createStorage, localhostRpc } from "@zeitgeistpm/sdk";
 import { Memory } from "@zeitgeistpm/web3.storage";
+import { KeyringPair } from '@polkadot/keyring/types.js';
 
 describe.only('zrml-prediction-markets Runtime Calls', function () {
   let api: ApiPromise;
-  let zeitgeistSDK;
+  let zeitgeistSDK: Sdk<RpcContext<MetadataStorage>, MetadataStorage>;
   let contract: ContractPromise;
   let process: ChildProcess;
-  let marketID: string;
 
   this.beforeAll(async function () {
     // process = startNode();
     await cryptoWaitReady();
     ({ api } = await getAPI());
-    // contract = await deployTestContract(api);
-    const SUDO = sudo();
+    contract = await deployTestContract(api);
 
     // Initialize Zeitgeist SDK for local
     zeitgeistSDK = await create({
       provider: ['ws://127.0.0.1:9944'],
       storage: createStorage(Memory.storage())
     });
+  });
 
-    // Creates a market
+  this.afterAll(async function () {
+    await api.disconnect();
+    // process.kill('SIGTERM');
+  });
+
+  async function createMarketWithOracle(signer: KeyringPair, api: ApiPromise, oracle: string) {
     const params: CreateMarketParams<typeof zeitgeistSDK> = {
       baseAsset: { Ztg: null },
-      signer: SUDO,
+      signer,
       disputeMechanism: "Authorized",
       marketType: { Categorical: 2 },
-      oracle: SUDO.address,
+      oracle,
       period: { Timestamp: [Date.now(), Date.now() + 60 * 60 * 24 * 1000 * 2] },
       deadlines: {
         disputeDuration: 5000,
-        gracePeriod: 200,
+        gracePeriod: 0, // NOTE: grace period is 0 so that dispute & report can happen rapidly
         oracleDuration: 500,
       },
       metadata: {
@@ -51,30 +56,89 @@ describe.only('zrml-prediction-markets Runtime Calls', function () {
         ],
         tags: ["dev"],
       },
-      // Something wrong with the pool I think
-      // pool: {
-      //   amount: ZTG.mul(300).toString(),
-      //   swapFee: "10000000",
-      //   spotPrices: ["50000000000"],
-      // },
       scoringRule: "Lmsr",
       creationType: "Permissionless"
     };
     const response = await zeitgeistSDK.model.markets.create(params);
     const marketCreatedEvent = response.raw.events.find(x => x.event.toHuman()['method'] === 'MarketCreated');
-    marketID = (marketCreatedEvent?.event?.toHuman()['data'] as any[])[0]
+    const marketID = (marketCreatedEvent?.event?.toHuman()['data'] as any[])[0];
+    await waitBlocks(api, 1);
+    return marketID;
+  }
+
+  // create market -> admin market close -> report
+  it('Should report a market if it is an oracle', async function () {
+    // Creates a market where the oracle is the contract
+    const SUDO = sudo();
+    const marketID = await createMarketWithOracle(SUDO, api, contract.address.toString());
+
+    // Sudo closes the market
+    const adminMoveMarketToClosedCall = api.tx.predictionMarkets.adminMoveMarketToClosed(marketID);
+    const sudoTx = api.tx.sudo.sudo(adminMoveMarketToClosedCall);
+    await sudoTx.signAndSend(SUDO);
+    await waitBlocks(api, 2);
+
+    // Smart contract disputes the market
+    let foundEvent = false;
+    const { gasRequired } = await contract.query.report(SUDO.address, maxWeight2(api), marketID, { Categorical: 1 });
+    await new Promise(async (resolve, _) => {
+      await contract.tx
+        .report(createGas(api, gasRequired), marketID, { Categorical: 1 })
+        .signAndSend(sudo(), async (res) => {
+          if (res.status.isInBlock) {
+            res.events.forEach(({ event: { data, method, section } }) => {
+              if (section === 'predictionMarkets' && method === 'MarketReported') {
+                foundEvent = true;
+              }
+            });
+            resolve(null);
+          }
+        });
+    });
+
+    expect(foundEvent).to.be.true;
   });
 
-  this.afterAll(async function () {
-    await api.disconnect();
-    // process.kill('SIGTERM');
-  });
+  // create market -> admin market close -> report -> dispute
+  it('Should be able to dispute a reported market', async function () {
+    // Creates a market where the oracle is the contract
+    const SUDO = sudo();
+    const marketID = await createMarketWithOracle(SUDO, api, SUDO.address.toString());
 
-  it.skip('Should log prediction market storage data', async function () {
-    const metadata = await api.rpc.state.getMetadata();
-    const predictionMarketsPallet = metadata.asLatest.pallets.find(x => x.name.toHuman() === 'PredictionMarkets');
-    if (predictionMarketsPallet && predictionMarketsPallet.storage) {
-      console.log(`Storage for PredictionMarkets:`, predictionMarketsPallet.storage.unwrap().toHuman());
-    }
+    // Sudo closes the market
+    const adminMoveMarketToClosedCall = api.tx.predictionMarkets.adminMoveMarketToClosed(marketID);
+    const sudoTx = api.tx.sudo.sudo(adminMoveMarketToClosedCall);
+    await sudoTx.signAndSend(SUDO);
+    await waitBlocks(api, 2);
+
+    // Sudo reports the market
+    const reportTx = api.tx.predictionMarkets.report(marketID, { Categorical: 1 });
+    await reportTx.signAndSend(SUDO);
+
+    // Gives contract DEV to reserve during dispute
+    const transfer = api.tx.balances.transfer(contract.address, 5_000_000_000_000n);
+    await transfer.signAndSend(new Keyring({ type: 'sr25519' }).addFromUri('//Alice'));
+    await waitBlocks(api, 2);
+
+    // Smart contract disputes the market
+    let foundEvent = false;
+    const { gasRequired } = await contract.query.dispute(SUDO.address, maxWeight2(api), marketID);
+    await new Promise(async (resolve, _) => {
+      await contract.tx
+        .dispute(createGas(api, gasRequired), marketID)
+        .signAndSend(sudo(), async (res) => {
+          if (res.status.isInBlock) {
+            res.events.forEach(({ event: { data, method, section } }) => {
+              if (section === 'predictionMarkets' && method === 'MarketDisputed') {
+                foundEvent = true;
+              }
+            });
+            resolve(null);
+          }
+        });
+    });
+
+    expect(foundEvent).to.be.true;
   });
 });
+
--- ztg_runtime_example/lib.rs
+++ ztg_runtime_example/lib.rs
@@ -5,7 +5,9 @@ mod ztg_runtime_example {
     use ink::env::Error as EnvError;
     use sp_runtime::Perbill;
     use ztg_runtime_lib::primitives::*;
-    use ztg_runtime_lib::runtime_structs::{AssetManagerCall, CourtCall, PredictionMarketsCall, RuntimeCall, StyxCall};
+    use ztg_runtime_lib::runtime_structs::{
+        AssetManagerCall, CourtCall, PredictionMarketsCall, RuntimeCall, StyxCall,
+    };
 
     #[derive(Debug, PartialEq, Eq, scale::Encode, scale::Decode)]
     #[cfg_attr(feature = "std", derive(scale_info::TypeInfo))]
@@ -43,14 +45,14 @@ mod ztg_runtime_example {
         }
 
         // region: Asset Manager
-        
+
         #[ink(message)]
         pub fn transfer(&mut self, dest: AccountId, amount: Balance) -> Result<()> {
             self.env()
-                .call_runtime(&RuntimeCall::AssetManager(AssetManagerCall::Transfer { 
-                    dest: dest.into(), 
-                    currency_id: ZeitgeistAsset::Ztg, 
-                    amount
+                .call_runtime(&RuntimeCall::AssetManager(AssetManagerCall::Transfer {
+                    dest: dest.into(),
+                    currency_id: ZeitgeistAsset::Ztg,
+                    amount,
                 }))
                 .map_err(Into::<Error>::into)
         }
@@ -60,39 +62,96 @@ mod ztg_runtime_example {
         // region: Court
 
         #[ink(message)]
-        pub fn join_court(&mut self, amount: Balance) -> Result<()>  {
+        pub fn join_court(&mut self, amount: Balance) -> Result<()> {
             self.env()
                 .call_runtime(&RuntimeCall::Court(CourtCall::JoinCourt { amount }))
                 .map_err(Into::<Error>::into)
         }
 
         #[ink(message)]
-        pub fn delegate(&mut self, amount: Balance, delegations: ink::prelude::vec::Vec<AccountId>) -> Result<()>  {
+        pub fn delegate(
+            &mut self,
+            amount: Balance,
+            delegations: ink::prelude::vec::Vec<AccountId>,
+        ) -> Result<()> {
             self.env()
-                .call_runtime(&RuntimeCall::Court(CourtCall::Delegate { amount, delegations }))
+                .call_runtime(&RuntimeCall::Court(CourtCall::Delegate {
+                    amount,
+                    delegations,
+                }))
                 .map_err(Into::<Error>::into)
         }
 
         #[ink(message)]
-        pub fn prepare_exit_court(&mut self) -> Result<()>  {
+        pub fn prepare_exit_court(&mut self) -> Result<()> {
             self.env()
                 .call_runtime(&RuntimeCall::Court(CourtCall::PrepareExitCourt))
                 .map_err(Into::<Error>::into)
         }
 
         #[ink(message)]
-        pub fn exit_court(&mut self, court_participant: AccountId) -> Result<()>  {
+        pub fn exit_court(&mut self, court_participant: AccountId) -> Result<()> {
             self.env()
-                .call_runtime(&RuntimeCall::Court(CourtCall::ExitCourt { 
-                    court_participant: court_participant.into() 
+                .call_runtime(&RuntimeCall::Court(CourtCall::ExitCourt {
+                    court_participant: court_participant.into(),
                 }))
                 .map_err(Into::<Error>::into)
         }
+
+        // TODO:
+        /*
+
+        #[codec(index = 4)]
+        Vote {
+            #[codec(compact)]
+            court_id: CourtId,
+            commitment_vote: Hash,
+        },
+        /// Denounce a juror during the voting period for which the commitment vote is known.
+        /// https://github.com/zeitgeistpm/zeitgeist/tree/release-v0.5.0/zrml/court/src/lib.rs#L784
+        #[codec(index = 5)]
+        DenounceVote {
+            #[codec(compact)]
+            court_id: CourtId,
+            juror: AccountId, // AccountIdLookupOf<T>
+            vote_item: VoteItem,
+            salt: Hash,
+        },
+        /// Reveal the commitment vote of the caller, who is a selected juror.
+        /// https://github.com/zeitgeistpm/zeitgeist/tree/release-v0.5.0/zrml/court/src/lib.rs#L868
+        #[codec(index = 6)]
+        RevealVote {
+            #[codec(compact)]
+            court_id: CourtId,
+            vote_item: VoteItem,
+            salt: Hash,
+        },
+        /// Initiate an appeal for a court
+        /// https://github.com/zeitgeistpm/zeitgeist/tree/release-v0.5.0/zrml/court/src/lib.rs#L957
+        #[codec(index = 7)]
+        Appeal {
+            #[codec(compact)]
+            court_id: CourtId,
+        },
+        /// Reassign the stakes of the jurors and delegators
+        /// https://github.com/zeitgeistpm/zeitgeist/tree/release-v0.5.0/zrml/court/src/lib.rs#L1046
+        #[codec(index = 8)]
+        ReassignCourtStakes {
+            #[codec(compact)]
+            court_id: CourtId,
+        },
+        /// Set the yearly inflation rate of the court system.
+        /// https://github.com/zeitgeistpm/zeitgeist/tree/release-v0.5.0/zrml/court/src/lib.rs#L1167
+        #[codec(index = 9)]
+        SetInflation { inflation: Perbill },
+
+        */
 
         // endregion
 
         // region: Styx
 
+        /// @note: Requires SUDO
         #[ink(message)]
         pub fn set_burn_amount(&mut self, amount: Balance) -> Result<()> {
             self.env()
@@ -109,38 +168,295 @@ mod ztg_runtime_example {
 
         // endregion
 
+        // region: Prediction Markets
+
+        /// @note: Requires SUDO
         #[ink(message)]
-        pub fn set_outcome_to_scalar_five(&mut self) {
-            self.outcome_report = OutcomeReport::Scalar(5);
+        pub fn admin_move_market_to_closed(&mut self, market_id: MarketId) -> Result<()> {
+            self.env()
+                .call_runtime(&RuntimeCall::PredictionMarkets(
+                    PredictionMarketsCall::AdminMoveMarketToClosed { market_id },
+                ))
+                .map_err(Into::<Error>::into)
         }
 
-        /// Returns the current value of the Flipper's boolean.
+        /// @note: Requires SUDO
         #[ink(message)]
-        pub fn get_outcome(&self) -> OutcomeReport {
-            self.outcome_report.clone()
+        pub fn admin_move_market_to_resolved(&mut self, market_id: MarketId) -> Result<()> {
+            self.env()
+                .call_runtime(&RuntimeCall::PredictionMarkets(
+                    PredictionMarketsCall::AdminMoveMarketToResolved { market_id },
+                ))
+                .map_err(Into::<Error>::into)
         }
 
-        pub fn create_market(&mut self) -> ink::env::Result<()> {
-            self.env().call_runtime(&RuntimeCall::PredictionMarkets(
-                PredictionMarketsCall::CreateMarket {
-                    base_asset: ZeitgeistAsset::Ztg,
-                    creator_fee: Perbill::zero(),
-                    oracle: self.env().account_id(),
-                    period: MarketPeriod::Block(Range { start: 5, end: 8 }),
-                    deadlines: Deadlines {
-                        grace_period: 0,
-                        oracle_duration: 0,
-                        dispute_duration: 0,
+        /// @note: Requires SUDO
+        #[ink(message)]
+        pub fn approve_market(&mut self, market_id: MarketId) -> Result<()> {
+            self.env()
+                .call_runtime(&RuntimeCall::PredictionMarkets(
+                    PredictionMarketsCall::ApproveMarket { market_id },
+                ))
+                .map_err(Into::<Error>::into)
+        }
+
+        /// @note: Requires SUDO
+        #[ink(message)]
+        pub fn request_edit(
+            &mut self,
+            market_id: MarketId,
+            edit_reason: ink::prelude::vec::Vec<u8>,
+        ) -> Result<()> {
+            self.env()
+                .call_runtime(&RuntimeCall::PredictionMarkets(
+                    PredictionMarketsCall::RequestEdit {
+                        market_id,
+                        edit_reason,
                     },
-                    metadata: MultiHash::Sha3_384([0; 50]),
-                    creation: MarketCreation::Permissionless,
-                    market_type: MarketType::Categorical(2),
-                    dispute_mechanism: None,
-                    scoring_rule: ScoringRule::Orderbook,
-                },
-            ))?;
-
-            Ok(())
+                ))
+                .map_err(Into::<Error>::into)
         }
+
+        #[ink(message)]
+        pub fn buy_complete_set(&mut self, market_id: MarketId, amount: Balance) -> Result<()> {
+            self.env()
+                .call_runtime(&RuntimeCall::PredictionMarkets(
+                    PredictionMarketsCall::BuyCompleteSet { market_id, amount },
+                ))
+                .map_err(Into::<Error>::into)
+        }
+
+        #[ink(message)]
+        pub fn dispute(&mut self, market_id: MarketId) -> Result<()> {
+            self.env()
+                .call_runtime(&RuntimeCall::PredictionMarkets(
+                    PredictionMarketsCall::Dispute { market_id },
+                ))
+                .map_err(Into::<Error>::into)
+        }
+
+        #[ink(message)]
+        pub fn create_market(
+            &mut self,
+            base_asset: ZeitgeistAsset,
+            creator_fee: Perbill,
+            oracle: AccountId,
+            period: MarketPeriod,
+            deadlines: Deadlines,
+            metadata: MultiHash,
+            creation: MarketCreation,
+            market_type: MarketType,
+            dispute_mechanism: Option<MarketDisputeMechanism>,
+            scoring_rule: ScoringRule,
+        ) -> Result<()> {
+            self.env()
+                .call_runtime(&RuntimeCall::PredictionMarkets(
+                    PredictionMarketsCall::CreateMarket {
+                        base_asset,
+                        creator_fee,
+                        oracle,
+                        period,
+                        deadlines,
+                        metadata,
+                        creation,
+                        market_type,
+                        dispute_mechanism,
+                        scoring_rule
+                    },
+                ))
+                .map_err(Into::<Error>::into)
+        }
+        
+        #[ink(message)]
+        pub fn edit_market(
+            &mut self,
+            base_asset: ZeitgeistAsset,
+            market_id: MarketId,
+            oracle: AccountId,
+            period: MarketPeriod,
+            deadlines: Deadlines,
+            metadata: MultiHash,
+            market_type: MarketType,
+            dispute_mechanism: Option<MarketDisputeMechanism>,
+            scoring_rule: ScoringRule,
+        ) -> Result<()> {
+            self.env()
+                .call_runtime(&RuntimeCall::PredictionMarkets(
+                    PredictionMarketsCall::EditMarket {
+                        base_asset,
+                        market_id,
+                        oracle,
+                        period,
+                        deadlines,
+                        metadata,
+                        market_type,
+                        dispute_mechanism,
+                        scoring_rule
+                    },
+                ))
+                .map_err(Into::<Error>::into)
+        }
+
+        #[ink(message)]
+        pub fn redeem_shares(&mut self, market_id: MarketId) -> Result<()> {
+            self.env()
+                .call_runtime(&RuntimeCall::PredictionMarkets(
+                    PredictionMarketsCall::RedeemShares { market_id },
+                ))
+                .map_err(Into::<Error>::into)
+        }
+
+        /// @note: Requires SUDO
+        #[ink(message)]
+        pub fn reject_market(&mut self, market_id: MarketId, reject_reason: ink::prelude::vec::Vec<u8>) -> Result<()> {
+            self.env()
+                .call_runtime(&RuntimeCall::PredictionMarkets(
+                    PredictionMarketsCall::RejectMarket { market_id, reject_reason },
+                ))
+                .map_err(Into::<Error>::into)
+        }
+
+        #[ink(message)]
+        pub fn report(&mut self, market_id: MarketId, outcome: OutcomeReport) -> Result<()> {
+            self.env()
+                .call_runtime(&RuntimeCall::PredictionMarkets(
+                    PredictionMarketsCall::Report { market_id, outcome },
+                ))
+                .map_err(Into::<Error>::into)
+        }
+
+        #[ink(message)]
+        pub fn sell_complete_set(&mut self, market_id: MarketId, amount: Balance) -> Result<()> {
+            self.env()
+                .call_runtime(&RuntimeCall::PredictionMarkets(
+                    PredictionMarketsCall::SellCompleteSet { market_id, amount },
+                ))
+                .map_err(Into::<Error>::into)
+        }
+
+        #[ink(message)]
+        pub fn start_global_dispute(&mut self, market_id: MarketId) -> Result<()> {
+            self.env()
+                .call_runtime(&RuntimeCall::PredictionMarkets(
+                    PredictionMarketsCall::StartGlobalDispute { market_id },
+                ))
+                .map_err(Into::<Error>::into)
+        }
+
+        #[ink(message)]
+        pub fn create_market_and_deploy_pool(
+            &mut self,
+            base_asset: ZeitgeistAsset,
+            creator_fee: Perbill,
+            oracle: AccountId,
+            period: MarketPeriod,
+            deadlines: Deadlines,
+            metadata: MultiHash,
+            market_type: MarketType,
+            dispute_mechanism: Option<MarketDisputeMechanism>,
+            amount: Balance,
+            spot_prices: ink::prelude::vec::Vec<Balance>,
+            swap_fee: Balance
+        ) -> Result<()> {
+            self.env()
+                .call_runtime(&RuntimeCall::PredictionMarkets(
+                    PredictionMarketsCall::CreateMarketAndDeployPool {
+                        base_asset,
+                        creator_fee,
+                        oracle,
+                        period,
+                        deadlines,
+                        metadata,
+                        market_type,
+                        dispute_mechanism,
+                        amount,
+                        spot_prices,
+                        swap_fee
+                    },
+                ))
+                .map_err(Into::<Error>::into)
+        }
+        
+        /// @note: Requires SUDO
+        #[ink(message)]
+        pub fn schedule_early_close(&mut self, market_id: MarketId) -> Result<()> {
+            self.env()
+                .call_runtime(&RuntimeCall::PredictionMarkets(
+                    PredictionMarketsCall::ScheduleEarlyClose { market_id },
+                ))
+                .map_err(Into::<Error>::into)
+        }
+
+        #[ink(message)]
+        pub fn dispute_early_close(&mut self, market_id: MarketId) -> Result<()> {
+            self.env()
+                .call_runtime(&RuntimeCall::PredictionMarkets(
+                    PredictionMarketsCall::DisputeEarlyClose { market_id },
+                ))
+                .map_err(Into::<Error>::into)
+        }
+
+        #[ink(message)]
+        pub fn reject_early_close(&mut self, market_id: MarketId) -> Result<()> {
+            self.env()
+                .call_runtime(&RuntimeCall::PredictionMarkets(
+                    PredictionMarketsCall::RejectEarlyClose { market_id },
+                ))
+                .map_err(Into::<Error>::into)
+        }
+
+        #[ink(message)]
+        pub fn close_trusted_market(&mut self, market_id: MarketId) -> Result<()> {
+            self.env()
+                .call_runtime(&RuntimeCall::PredictionMarkets(
+                    PredictionMarketsCall::CloseTrustedMarket { market_id },
+                ))
+                .map_err(Into::<Error>::into)
+        }
+
+        #[ink(message)]
+        pub fn manually_close_market(&mut self, market_id: MarketId) -> Result<()> {
+            self.env()
+                .call_runtime(&RuntimeCall::PredictionMarkets(
+                    PredictionMarketsCall::ManuallyCloseMarket { market_id },
+                ))
+                .map_err(Into::<Error>::into)
+        }
+
+        // endregion
+
+        // #[ink(message)]
+        // pub fn set_outcome_to_scalar_five(&mut self) {
+        //     self.outcome_report = OutcomeReport::Scalar(5);
+        // }
+
+        // /// Returns the current value of the Flipper's boolean.
+        // #[ink(message)]
+        // pub fn get_outcome(&self) -> OutcomeReport {
+        //     self.outcome_report.clone()
+        // }
+
+        // pub fn create_market(&mut self) -> ink::env::Result<()> {
+        //     self.env().call_runtime(&RuntimeCall::PredictionMarkets(
+        //         PredictionMarketsCall::CreateMarket {
+        //             base_asset: ZeitgeistAsset::Ztg,
+        //             creator_fee: Perbill::zero(),
+        //             oracle: self.env().account_id(),
+        //             period: MarketPeriod::Block(Range { start: 5, end: 8 }),
+        //             deadlines: Deadlines {
+        //                 grace_period: 0,
+        //                 oracle_duration: 0,
+        //                 dispute_duration: 0,
+        //             },
+        //             metadata: MultiHash::Sha3_384([0; 50]),
+        //             creation: MarketCreation::Permissionless,
+        //             market_type: MarketType::Categorical(2),
+        //             dispute_mechanism: None,
+        //             scoring_rule: ScoringRule::Orderbook,
+        //         },
+        //     ))?;
+
+        //     Ok(())
+        // }
     }
 }
